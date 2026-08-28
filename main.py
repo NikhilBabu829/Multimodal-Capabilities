34dc6a2442d60be71715c040660ab4b29c84641f
macbook-m4 <87521416+NikhilBabu829@users.noreply.github.com>
2026-08-28 20:21:38 +0100

    the vision part of the application is completed, now i need to move towards voice

--- main.py
+++ main.py
@@ -9,18 +9,28 @@ from dotenv import load_dotenv
 load_dotenv()
 con = console.Console()
 
-def encode_file(file_path : str):
-    with open(file_path, "rb") as file : 
-        encoded_image = base64.b64encode(file.read().decode("utf-8"))
-    if file_path.endswith(".png"):
-        image_media_type = "image/png"
-    elif file_path.endswith(".jpg"):
-        image_media_type = "image/jpg"
-    elif file_path.endswith(".jpeg"):
-        image_media_type = "image/jpeg"
-    return encoded_image, image_media_type
+IS_READY = False
+ENCODED_IMAGE = ""
+IMAGE_MEDIA_TYPE = ""
 
-def sendingToAi(user_query : str, encoded_data, image_media_type):
+def encode_file(file_path : str):
+    con.log("We are now trying to encode the file")
+    con.log(f"The file path given is {file_path}")
+    global ENCODED_IMAGE, IMAGE_MEDIA_TYPE, IS_READY
+    with open(file_path, "rb") as file :
+        ENCODED_IMAGE = base64.b64encode(file.read()).decode("utf-8")
+    if file_path.endswith(".png"):
+        IMAGE_MEDIA_TYPE = "image/png"
+    elif file_path.endswith(".jpg"):
+        IMAGE_MEDIA_TYPE = "image/jpg"
+    elif file_path.endswith(".jpeg"):
+        IMAGE_MEDIA_TYPE = "image/jpeg"
+    IS_READY = True
+    con.print("We are ready for your question about the image")
+    return True
+
+def sendingToAi(user_query : str):
+    global ENCODED_IMAGE, IMAGE_MEDIA_TYPE
     api_key = os.getenv("API_KEY")
     headers = {
         "x-api-key": api_key,
@@ -29,23 +39,24 @@ def sendingToAi(user_query : str, encoded_data, image_media_type):
     }
     payload = {
         "model": "claude-haiku-4-5-20251001",
-        "max_tokens": 500,
+        "max_tokens": 1000,
         "messages": [{"role": "user", "content": [
             {
                 "type" : "image", 
                 "source" : {
                     "type" : "base64",
-                    "media_type" : image_media_type,
-                    "data" : encoded_data
+                    "media_type" : IMAGE_MEDIA_TYPE,
+                    "data" : ENCODED_IMAGE
                 }
             }, 
             {"type" : "text", "text" : user_query}
         ]}]
     }
     try : 
-        with httpx.Client as client:
-            api_response = client.post("https://api.anthropic.com/v1/messages", headers = headers, payload=payload)
+        with httpx.Client() as client:
+            api_response = client.post("https://api.anthropic.com/v1/messages", headers = headers, json=payload)
             response_in_json = api_response.json()
+            print(response_in_json)
     except httpx.HTTPError as err:
         print("Exception Caused For ", err)
 
@@ -54,8 +65,22 @@ def main():
     con.log("Starting the application")
     print("This application will take in a photo and then answer your questions based on the photo you gave it.")
     print("Please type in or past the photo path into the textfield")
+    global IS_READY
     while True:
-        input = Prompt.ask("")
+        try:
+            input = Prompt.ask("")
+            if input.strip() == "":
+                continue
+            if IS_READY:
+                con.log("Sending the image information and the user query to the Ai for answers")
+                sendingToAi(input)
+                continue
+            should_continue = encode_file(input)
+            if not should_continue:
+                break
+        except KeyboardInterrupt:
+            con.print("\nExiting the program. Goodbye!", style="bold red")
+            break
 
 if __name__ == "__main__":
     main()
